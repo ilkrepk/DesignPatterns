60d69cff73e24d02b0d54231d84d52103306a9ba
ilkrepk <epikilker@gmail.com>
2019-04-24 10:58:19 +0300

    Lock objesi kullanılarak Thread Safe eklendi.

--- DesignPatterns/Singleton_01/Program.cs
+++ DesignPatterns/Singleton_01/Program.cs
@@ -20,6 +20,8 @@ namespace Singleton_01
     class CustomerManager
     {
         static CustomerManager _customerManager;
+        //Eğer iki kullanıcı aynı anda istekte bulunursa bu bir çakışmaya sebep olup, 2 tana nesene oluşmasına sebep olabilir.  Bu sebepten dolayı bir lock objesi oluşturup CreateAsSingleton metodu içerisinde nesne oluşturma ilemini lock içine yapıp bu işlemi kilitlemiş olup bir thread safe oluşturulmuş olduk.
+        static object _lockObject = new object();
         //Constructor' ı olan ancak dışarıdan erişilemeyen.
         private CustomerManager()
         {
@@ -27,10 +29,13 @@ namespace Singleton_01
         }
         public static CustomerManager CreateAsSingleton()
         {
-            //Burada oluşturulmuş bir nesne var mı kontrolü yapılması gerekmekte. Eğer oluşturulmuş bir nesne varsa onu döndüreceğiz yok ise bir nesne oluşturmamız gerekmekte.
-            if (_customerManager==null)
+            //Burada oluşturulmuş bir nesne var mı kontrolü yapılması gerekmekte. Eğer oluşturulmuş bir nesne varsa onu döndüreceğiz yok ise bir nesne oluşturmamız gerekmekte. _lockObject olayını yukarıdaki açıklama satırında yazdım.
+            lock (_lockObject)
             {
-                _customerManager = new CustomerManager();
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
             }
 
             return _customerManager;
