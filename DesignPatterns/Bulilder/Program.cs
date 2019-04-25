5d6203feead344d6182701463eec4cac5bb910db
ilkrepk <epikilker@gmail.com>
2019-04-25 15:44:20 +0300

    Builder desen projesi eklendi.

---
+++ DesignPatterns/Bulilder/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulilder
+{
+    //Bir siteye giriş yapacan kullanıcıya gösterilecek ürünlerin türüne göre bir model nesnesini değiştirilmesi üzerine bir örnek ile bu deseni tasarlayacağız.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirector director = new ProductDirector();
+            var builder = new NewCustomerProductBuilder();
+            director.GenerateProduct(builder);
+            var model = builder.GetModel();
+            Console.WriteLine(model.Id);
+            Console.WriteLine(model.CategoryName);
+            Console.WriteLine(model.DiscountApplied);
+            Console.WriteLine(model.DiscountedPrice);
+            Console.WriteLine(model.UnitPrice);
+            Console.WriteLine(model.ProductName);
+
+            Console.ReadLine();
+        }
+    }
+    class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public bool DiscountApplied { get; set; }
+    }
+
+    //Public olmamasının sebebi hepsi aynı namespace altında olduğundan
+    abstract class ProductBuilder
+    {
+        public abstract void GetProductData();
+        public abstract void ApplyDiscount();
+        public abstract ProductViewModel GetModel();
+    }
+
+    //Daha önce bizden alışveriş yapmammış müşteri, bu müşteriye ilk sipariş için indirim uygulanabilir.
+    class NewCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverage";
+            model.ProductName="Chai";
+            model.UnitPrice = 20;
+        }
+
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice*(decimal)0.90;
+            model.DiscountApplied = true;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+    //Bizden daha önce alışveriş yapmış müşteri
+    class OldCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverage";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+        }
+
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice;
+            model.DiscountApplied = false;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+    class ProductDirector
+    {
+        public void GenerateProduct(ProductBuilder productBuilder)
+        {
+            productBuilder.GetProductData();
+            productBuilder.ApplyDiscount();
+        }
+    }
+
+}
